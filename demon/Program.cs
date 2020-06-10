29fc0f776edcbe5d1b5ae9167b9d41fab8209893
hang <dinghang@ipplus360.com>
2020-06-10 17:31:40 +0800

    优化示例代码

--- demon/Program.cs
+++ demon/Program.cs
@@ -10,7 +10,7 @@ namespace demon
     {
         static void Main(string[] args)
         {
-            using (var reader = new Reader("C:\\Users\\Administrator\\Desktop\\IP_city_2020W16_single_WGS84.awdb"))
+            using (var reader = new Reader("C:\\Users\\Administrator\\Desktop\\awdb.awdb"))
             {
                 var metadata = reader.Metadata;
 
@@ -18,18 +18,34 @@ namespace demon
                 var ip = IPAddress.Parse("166.111.4.100");
                 var data = reader.Find<Dictionary<string, object>>(ip);
 
-                Console.WriteLine("精度:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["accuracy"]));
-                Console.WriteLine("areacode:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["areacode"]));
-                Console.WriteLine("asnumber:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["asnumber"]));
 
-                Console.WriteLine("continent:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["continent"]));
-                Console.WriteLine("country:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["country"]));
-                Console.WriteLine("isp{0}:", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["isp"]));
-                Console.WriteLine("timezone:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["timezone"]));
+                Console.WriteLine("洲:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["continent"]));
+                Console.WriteLine("国家编码:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["areacode"]));
+                Console.WriteLine("国家:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["country"]));
+                Console.WriteLine("邮编:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["zipcode"]));
+                Console.WriteLine("时区:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["timezone"]));
+                Console.WriteLine("定位精度:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["accuracy"]));
+                Console.WriteLine("定位方式:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["source"]));
+            
 
-                Console.WriteLine("zipcode:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["zipcode"]));
+                List<object> array = (List<object>)data["multiAreas"];
+                var prov = ((Dictionary<string, object>)array[0])["prov"];
+                var city = ((Dictionary<string, object>)array[0])["city"];
+                var district = ((Dictionary<string, object>)array[0])["district"];
+                var lat = ((Dictionary<string, object>)array[0])["latwgs"];
+                var lng = ((Dictionary<string, object>)array[0])["lngwgs"];
+                var radius = ((Dictionary<string, object>)array[0])["radius"];
+                Console.WriteLine("省:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])prov));
+                Console.WriteLine("城市:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])city));
+                Console.WriteLine("区县:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])district));
+                Console.WriteLine("纬度:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])lat));
+                Console.WriteLine("经度:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])lng));
+                Console.WriteLine("半经:{0}", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])radius));
 
-               
+
+                Console.WriteLine("运营商{0}:", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["isp"]));
+                Console.WriteLine("AS号{0}:", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["asnumber"]));
+                Console.WriteLine("拥有者{0}:", System.Text.UTF8Encoding.UTF8.GetString((System.Byte[])data["owner"]));
 
 
             }
